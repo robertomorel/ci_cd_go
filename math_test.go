9b6db9f18036162be8a7f90e5bf0f385ecac06ef
Roberto Morel <roberto.morel@oowlish.com>
2022-08-20 20:14:17 -0300

    Added pipeline

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := Sum(15, 1)
+	total := Sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Invalid sum result: Result: %d. Expected: %d", total, 30)
